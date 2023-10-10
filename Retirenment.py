5414180067954be657421f40331c5a815a4749c7
ElianGuerrero <145053455+ElianGuerrero@users.noreply.github.com>
2023-10-09 19:43:36 -0600

    Create Retirenment.py

---
+++ Retirenment.py
@@ -0,0 +1,10 @@
+print("Retierment calculator")
+current_age = int(input("Enter your current age: "))
+retirement_age = int(input("Enter the age you want to retire: "))
+desired_retirement_amount = int(input("Enter your desired retirement amount: "))
+P = retirement_age-current_age
+G = P * 12
+
+monthly_savings = (int(desired_retirement_amount))/G
+
+print("You need to save about", monthly_savings, "per month, to reach your retirement goal")
