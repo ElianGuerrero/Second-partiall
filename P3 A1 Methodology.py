c8bca6e11602b8e6ce160070a97cfb37f6023e9d
ElianGuerrero <145053455+ElianGuerrero@users.noreply.github.com>
2023-10-30 14:34:23 -0600

    Create P3 A1 Methodology.py

---
+++ P3 A1 Methodology.py
@@ -0,0 +1,35 @@
+#P3 A1 Methodology
+https://replit.com/join/ykpdxfubsc-elianguerrero1
+
+print("Hi, today we are going to make a check list of your week, and depending on your answers I will give you some tips!")
+exercise = input("Do you exirsice often? answer yes or no")
+sleep = input("Do you sleep 8 hours a day? answer yes or no")
+study = input("Do you study an hour a day? answer yes or no")
+socialize = input("Do you go out with friends? answer yes or no")
+eat = input("Do you eat the 3 meals of the day everyday? answer yes or no")
+
+if exercise == "yes":
+  print("Keep doing exercise!")
+elif exercise == "no":
+  print("You should exercise more!")
+  
+if sleep == "yes":
+  print("Keep sleeping 8 hours!")
+elif sleep == "no":
+  print("You should sleep more!")
+
+if study == "yes":
+  print("Keep studiyng!")
+elif study == "no":
+  print("You should study more!")
+
+if socialize == "yes":
+  print("Keep socializing!")
+elif socialize == "no":
+  print("You should socialize more!")
+
+if eat == "yes":
+  print("Keep eating!")
+elif eat == "no":
+  print("You should eat more!")
+
