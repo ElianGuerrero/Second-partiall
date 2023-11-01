6062611334ed9ae789e228daeecb5d5556132d26
ElianGuerrero <145053455+ElianGuerrero@users.noreply.github.com>
2023-11-01 16:37:05 -0600

    Create Zodiacsing.py

---
+++ Zodiacsing.py
@@ -0,0 +1,65 @@
+https://replit.com/join/wlmsoltauu-elianguerrero1
+
+print("Hi, today we are going to see what zodiac sign you are, please answer the next questions:")
+day =int(input("What is your birth DAY?")) 
+month = int(input("What is your birth month?, please put the number of the month."))
+
+if month == 12 and (day >= 22 and day <=31):
+  print("Capricorn")
+elif month == 1 and (day >= 1 and day <=19):
+  print("Capricorn")
+
+elif month == 1 and (day >= 20 and day <= 31):
+  print("Aquarius")
+elif month == 2 and (day >= 1 and day <= 18):
+  print("Aquarius")
+
+elif month == 2 and (day >= 19 and day <= 29):
+  print("Pisces")
+elif month == 3 and (day >= 1 and day <= 20):
+  print("Pisces")
+
+elif month == 3 and (day >= 21 and day <= 31):
+  print("Aries")
+elif month == 4 and (day >= 1 and day <= 19):
+  print("Aries")
+
+elif month == 4 and (day >= 20 and day <= 31):
+  print("Taurus")
+elif month == 5 and (day >= 1 and day <= 20):
+  print("Taurus")
+
+elif month == 5 and (day >= 21 and day <= 31):
+  print("Gemini")
+elif month == 6 and (day >= 1 and day <= 20):
+  print("Gemini")
+
+elif month == 6 and (day >= 21 and day <= 30):
+  print("Cancer")
+elif month == 7 and (day >= 1 and day <= 22):
+  print("Cancer")
+
+elif month == 7 and (day >= 23 and day <= 31):
+  print("Leo")
+elif month == 8 and (day >= 1 and day <= 22):
+  print("Leo")
+
+elif month == 8 and (day >= 23 and day <= 31):
+  print("Virgo")
+elif month == 9 and (day >= 1 and day <= 22):
+  print("Virgo")
+
+elif month == 9 and (day >= 23 and day <= 30):
+  print("Libra")
+elif month == 10 and (day >= 1 and day <= 22):
+  print("Libra")
+
+elif month == 10 and (day >= 23 and day <= 31):
+  print("Scorpio")
+elif month == 11 and (day >= 1 and day <= 21):
+  print("Scorpio")
+
+elif month == 11 and (day >= 22 and day <= 30):
+  print("Sagittarius")
+elif month == 12 and (day >= 1 and day <= 21):
+  print("Sagittarius")
